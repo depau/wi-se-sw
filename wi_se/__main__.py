37ba6832a6ed8323058bb264d5117417b0b5ac4b
Davide Depau <davide@depau.eu>
2021-01-25 19:56:38 +0100

    Add __main__ for CPython

---
+++ wi_se/__main__.py
@@ -0,0 +1,10 @@
+# This file is expected to be run on normal CPython for testing, with stubs installed
+# python -m wi_se
+#
+# This will not run on MicroPython
+
+
+from . import main
+
+if __name__ == '__main__':
+    main()
